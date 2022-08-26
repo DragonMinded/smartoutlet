d436091a9bafe29a95001618b422fd1dfd6ec50e
Jennifer Taylor <dragonminded@dragonminded.com>
2022-08-26 22:55:06 +0000

    Port over NP-02B undocumented hack for controlling units that don't follow the protocol specified in the manual.

--- smartoutlet/np_02b.py
+++ smartoutlet/np_02b.py
@@ -1,4 +1,5 @@
 import requests
+import xml.etree.ElementTree as ET
 from typing import ClassVar, Dict, Optional, cast
 
 from .interface import OutletInterface
@@ -39,20 +40,55 @@ class NP02BOutlet(OutletInterface):
 
     def getState(self) -> Optional[bool]:
         try:
-            response = requests.get(f"http://{self.username}:{self.password}@{self.host}/cmd.cgi?$A5", timeout=1.0).content.decode('utf-8')
+            response = requests.get(
+                f"http://{self.username}:{self.password}@{self.host}/cmd.cgi?$A5",
+                timeout=1.0,
+            ).content.decode('utf-8').strip()
         except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
+            return None
+
+        # There are two types of response here, if it returns "Success!" then
+        # it doesn't respond to the correct documented protocol.
+        if response == "Success!":
+            relay = f"rly{self.outlet - 1}"
+            response = requests.get(
+                f"http://{self.username}:{self.password}@{self.host}/status.xml",
+                timeout=1.0
+            ).content.decode('utf-8')
+
+            root = ET.fromstring(response)
+            if root.tag == "response":
+                for child in root:
+                    if child.tag == relay:
+                        return child.text != '0'
             return None
 
         if '$' in response:
             return None
-        if self.outlet == 1:
-            return response[1] != '0'
-        if self.outlet == 2:
-            return response[0] != '0'
-        return None
+        if len(response) < (self.outlet - 1):
+            return None
+        return response[-self.outlet] != '0'
 
     def setState(self, state: bool) -> None:
         try:
-            requests.get(f"http://{self.username}:{self.password}@{self.host}/cmd.cgi?$A3 {self.outlet} {'1' if state else '0'}", timeout=1.0)
+            response = requests.get(
+                f"http://{self.username}:{self.password}@{self.host}/cmd.cgi?$A3 {self.outlet} {'1' if state else '0'}",
+                timeout=1.0
+            ).content.decode('utf-8').strip()
         except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
-            pass
+            return
+
+        if response == "Success!":
+            # This outlet is not responding to the correct documented protocol,
+            # we must query the status and then flip the relay if needed.
+            actual = self.getState()
+            if actual is None:
+                # Couldn't query, so don't want to mess with toggling the relay.
+                return
+
+            if actual != state:
+                try:
+                    # Need to toggle
+                    requests.get(f"http://{self.username}:{self.password}@{self.host}/cmd.cgi?rly={self.outlet - 1}", timeout=1.0)
+                except (requests.exceptions.ConnectTimeout, requests.exceptions.ConnectionError):
+                    pass
